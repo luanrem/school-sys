83acb2e26cf59806bb9373ed35e5a5a9591a5cfb
luanrem <luanrem@gmail.com>
2025-04-19 18:14:39 -0300

    create student table

--- frontend/src/app/students/index.tsx
+++ frontend/src/app/students/index.tsx
@@ -1,5 +1,20 @@
+import { columns, Student } from "@/components/student-table/columns";
+import { DataTable } from "@/components/student-table/data-table";
+
+
 export default function Students() {
+  const data = [
+    {
+      id: 1,
+      firstName: 'banana',
+      lastName: "pending",
+      email: "m@example.com",
+    },
+  ]
+
   return (
-    <div>Student</div>
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
   )
 }
---
+++ frontend/src/components/student-table/columns.tsx
@@ -0,0 +1,29 @@
+import { ColumnDef } from "@tanstack/react-table"
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Student = {
+  id: number
+  firstName: string
+  lastName: string
+  email: string
+}
+
+export const columns: ColumnDef<Student>[] = [
+  {
+    accessorKey: "id",
+    header: "Id",
+  },
+  {
+    accessorKey: "firstName",
+    header: "First Name",
+  },
+  {
+    accessorKey: "lastName",
+    header: "Last Name",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+]
