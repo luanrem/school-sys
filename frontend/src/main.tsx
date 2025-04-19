5e54c05d0471ce74264e7559468c4d0656952383
luanrem <luanrem@gmail.com>
2025-04-19 17:25:20 -0300

    create sideBar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar.tsx'
+import { AppSidebar } from './components/app.sidebar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        <App />
+      </main>
+    </SidebarProvider>
+
   </StrictMode>,
 )
